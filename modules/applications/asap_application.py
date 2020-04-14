70690c72d896989503695970b130b62762f91fa4
Harold Meerwaldt <haroldmeerwaldt@hotmail.com>
2020-04-14 20:23:18 +0200

    added toolbox with PandasModel

--- modules/applications/asap_application.py
+++ modules/applications/asap_application.py
@@ -1,5 +1,7 @@
 from PySide2 import QtWidgets
 from modules.GUI_layouts import asap_layout
+from modules.utilities import toolbox
+import pandas as pd
 
 
 class ASAPApplication(QtWidgets.QMainWindow):
@@ -11,3 +13,9 @@ class ASAPApplication(QtWidgets.QMainWindow):
         self.ui = asap_layout.Ui_MainWindow()
         self.ui.setupUi(self)
         print(vars(self.ui))
+
+        df = pd.DataFrame({'a': [2, 4], 'b': [3, 5]})
+
+        model = toolbox.PandasModel(df)
+
+        self.ui.tableView.setModel(model)
